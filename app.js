64f1b81d638f610520c2c7f9c77e990036c87846
HassanKhan123 <hk2602809@gmail.com>
2020-11-03 08:31:28 +0500

    using v-model for two way binding

--- app.js
+++ app.js
@@ -2,25 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name:'',
-      confirmedName:''
+      name:''
     };
   },
   methods:{
-    submitForm(e){
-      alert('Submitted!')
-    },
-    confirmInput(){
-      this.confirmedName = this.name
-    },
-    setName(event){
-      this.name=event.target.value
-    },
     add(num){
       this.counter=this.counter+num
     },
     reduce(num){
       this.counter=this.counter-num
+    },
+    resetInput(){
+      this.name=""
     }
   }
 });
