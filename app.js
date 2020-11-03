8da42460e3e1f78196a421b9f7b621cdf00d26bb
HassanKhan123 <hk2602809@gmail.com>
2020-11-03 08:11:03 +0500

    event modifiers added

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name:''
+      name:'',
+      confirmedName:''
     };
   },
   methods:{
+    submitForm(e){
+      alert('Submitted!')
+    },
+    confirmInput(){
+      this.confirmedName = this.name
+    },
     setName(event){
       this.name=event.target.value
     },
